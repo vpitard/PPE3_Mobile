86b19202ca4c8733fb715bc3357637f3f1ab6d0c
maillard <mm6195@hotmail.fr>
2016-11-21 14:29:24 +0100

    Debut de l'ecran

--- app/src/main/java/com/example/vpitard/ppe3_mobile/MainActivity.java
+++ app/src/main/java/com/example/vpitard/ppe3_mobile/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.vpitard.ppe3_mobile;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +13,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //* test push mael*//
+        Button btnConnexion = (Button) findViewById(R.id.btnConnexion);
+        btnConnexion.setOnClickListener(observateurclic);
+    }
 
+    public View.OnClickListener observateurclic = new View.OnClickListener() {
+        public void onClick(View v) {
+            String Login = String.valueOf(((EditText) findViewById(R.id.LoginTxt)).getText());
+            String Pwd = String.valueOf(((EditText) findViewById(R.id.PwdTxt)).getText());
+        }
     }
 }
+
